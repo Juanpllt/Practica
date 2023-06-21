8dd18f4a19df46da5f2ea18a58ec88df910cf3a9
Juanpllt <juanpablotobon53@gmail.com>
2023-06-20 23:17:58 -0500

    comprobando

--- main/Practica.java
+++ main/Practica.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Practica {
     
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);    
-        
+        Scanner sc = new Scanner(System.in);    
+        a
     }
     
 }
